5dc4ed5ee0f7289b04a138a5916d17b509e35203
Yang Vang <lanceyvang@gmail.com>
2019-01-05 23:48:38 -0500

    finished routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,46 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './components/App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import './index.css';
+
+import 'gestalt/dist/gestalt.css';
+
+import App from './Components/App';
+// import Navbar from './Components/Navbar';
+import Signin from './Components/Signin';
+import Signup from './Components/Signup';
+import Checkout from './Components/Checkout';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+	<Router>
+		<React.Fragment>
+			{/* <Navbar /> */}
+			<Switch>
+				{/* Use exact path to not match multiple routes  */}
+				<Route component={App} exact path='/' />
+				<Route component={Signin} path='/signin' />
+				<Route component={Signup} path='/signup' />
+				<Route component={Checkout} path='/checkout' />
+			</Switch>
+		</React.Fragment>
+	</Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
+
+//updates the page without a reload
+if (module.hot) {
+	module.hot.accept();
+}
