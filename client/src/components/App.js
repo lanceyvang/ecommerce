98ebb363899b0cc66cbe451b796bb2cd36f5424f
Yang Vang <lanceyvang@gmail.com>
2019-01-06 01:01:39 -0500

    updated homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,3 @@
-// import React, { Component } from 'react';
-// import './App.css';
-
-// class App extends Component {
-// 	render() {
-// 		return <div>App</div>;
-// 	}
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 // prettier-ignore
 import { Container, Box, Heading, Card, Image, Text, SearchField, Icon, Spinner } from 'gestalt';
