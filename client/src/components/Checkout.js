323d2539e1c0c9931d7f8b68e45e1e0d0211c751
Yang Vang <lanceyvang@gmail.com>
2019-01-07 00:55:06 -0500

    finished forms in signup

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Box, Button, Heading, Text, TextField } from 'gestalt';
 
 class Checkout extends React.Component {
 	render() {
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,65 @@
 import React from 'react';
+import { Container, Box, Button, Heading, Text, TextField } from 'gestalt';
 
 class Signup extends React.Component {
+	state = {
+		username: '',
+		email: '',
+		password: ''
+	};
+
+	handleChange = ({ event, value }) => {
+		event.persist();
+		this.setState({ [event.target.name]: value });
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		if (!this.isFormEmpty(this.state)) {
+			console.log('submitted');
+		}
+	};
+
+	isFormEmpty = ({ username, email, password }) => {
+		return !username || !email || !password;
+	};
 	render() {
-		return <div>Signup</div>;
+		return (
+			<Container>
+				<Box
+					dangerouslySetInlineStyle={{ __style: { backgroundColor: '#ebe2da' } }}
+					margin={4}
+					padding={4}
+					shape='rounded'
+					display='flex'
+					justifyContent='center'
+				>
+					{/* Sign Up Form */}
+					<form
+						style={{
+							display: 'inline',
+							textAlign: 'center',
+							maxWidth: 450
+						}}
+						onSubmit={this.handleSubmit}
+					>
+						<Box marginBottom={2} display='flex' direction='column' alignItems='center'>
+							<Heading color='midnight'>Let's Get Started</Heading>
+							<Text italic color='orchid'>
+								Sign up to order some brews!
+							</Text>
+						</Box>
+						{/* Username Input */}
+						<TextField id='username' type='text' name='username' placeholder='Username' onChange={this.handleChange} />
+						{/* Email Address */}
+						<TextField id='email' type='text' name='email' placeholder='Email Address' onChange={this.handleChange} />
+						{/* Password Input */}
+						<TextField id='password' type='text' name='password' placeholder='Password' onChange={this.handleChange} />
+						<Button inline color='blue' text='submit' type='submit' />
+					</form>
+				</Box>
+			</Container>
+		);
 	}
 }
 
