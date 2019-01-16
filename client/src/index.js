44d5ca2d1153e177471e8ec4e3383176d4fe35c0
Yang Vang <lanceyvang@gmail.com>
2019-01-16 04:05:06 -0500

    updated checkout component

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-// import './index.css';
 import { getToken } from './Utils';
 import 'gestalt/dist/gestalt.css';
 
